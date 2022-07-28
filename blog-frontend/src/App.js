921a3061f756c88b4642c6a517d0d60fdcef14bd
Jiuk Choi <jiukchoi@algotradingsysteminc.com>
2022-07-27 21:06:51 -0500

    Update App.js
    no more bootstrap

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Post from './pages/Post';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <nav>
 
-    </div>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/post' element={<Post />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+
   );
 }
 
